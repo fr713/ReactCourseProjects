80b93d4074f7e03bf1fbf7eca05081a25d854a89
fr713 <96714471+fr713@users.noreply.github.com>
2023-01-03 20:22:35 -0600

    Add files via upload
    This project is a silly project where we have a button where we can add animals and once that button is pressed, the website undergoes a state change and once the website re-renders, a picture of a random animal is shown on the screen with a heart attached to the animal. If the user clicks on the animal, then the heart grows larger and larger. The user can keep adding as many animals as they like all with hearts attached to the animals.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
+import './App.css'
 import { useState } from 'react';
-import searchImages from './api';
-import ImageList from './components/ImageList';
-import SearchBar from './components/SearchBar'
+import AnimalShow from './AnimalShow'
 
-function App(){
-    const [images, setImages] = useState([]);
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
 
-    const handleSubmit = async (term) => {
-        const result = await searchImages(term);
-        //we want to access result after searchImages finishes but without "await", result doesnt know to wait
-        setImages(result);
-    };
-
-    return (
-        <div>
-            <SearchBar onSubmit = {handleSubmit} />
-            <ImageList images={images}/>
-        </div>
-    );
+    return animals[Math.floor(Math.random() * animals.length)];
 }
 
+function App(){
+    const [animals, setAnimals] = useState([])
+    //useState(0) is a function that returns a two element array with count = 0
+    //The first element is the piece of data that is going to change over time
+    //The second element is a function that is in charge of re-rendering (calling App()) the screen
+    //setCount is a variable that represents this function
+    //setCount takes in a parameter for count and updates all instances of count while re-rendering
+
+    // NEVER GOING TO DO THIS:
+    //count = count + 1 HAVE TO USE SETCOUNT FUNCTION
+
+    const handleClick = () => {
+        //handleClick is a variable that represents a function
+        setAnimals([...animals, getRandomAnimal()])
+    };
+
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key = {index} />
+        // equivalent to const renderedAnimals =  <AnimalShow type={animals[0]}/>
+        //                                        <AnimalShow type={animals[1]}/>
+    });
+
+    return (
+        <div className = "app"> 
+            <button onClick = {handleClick}> Add Animal </button>
+            <div className='animal-list'> {renderedAnimals} </div>
+         </div>
+    );
+}
 export default App;
+
+//const doGreeting = () => {return hi there}
+//1. console.log( doGreeting ) **reference to the function so that when we do click, it is called**
+//v.s.
+//2. console.log( doGreeting() )
+// 1 logs the doGreeting function but does not call it
+// 2 immediately calls and in the onClick situation, 2 calls the function while
+// the code is being rendered/displayed because once React interprets that code even if a 
+//button is not pressed, it still calls on it
+// For 2, the button gets the return value
+// 1 is a reference to the function while 2 is the actual value that the function returns.
+// In order for buttons to work when one presses on it, it needs a function that it can rely on when
+// someone preeses the button
+
+//<button onClick = {handleClick}> Add Animal </button> is the same ass
+// <button onClick = {() => console.log('Button was clicked')}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import SearchImages from './api';
-import searchImages from './api';
-
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
